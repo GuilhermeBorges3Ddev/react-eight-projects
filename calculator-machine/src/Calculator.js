74460d5891d30d418442cc587bf1ab91f6e4cb9d
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-01-29 23:20:16 -0300

    Calculator layout done

--- calculator-machine/src/Calculator.js
+++ calculator-machine/src/Calculator.js
@@ -1,7 +1,90 @@
+import { Jumbotron, Container, Row, Col, Button, Form } from "react-bootstrap";
 import "./Calculator.css";
 
+const CalculatorJumbotronStyles = {
+  background: "transparent !important",
+  backgroundColor: "#007bff",
+  padding: "1rem",
+  margin: "1rem",
+  width: "40rem",
+};
+
 function Calculator() {
-  return <h1>lala</h1>;
+  return (
+    <Jumbotron style={CalculatorJumbotronStyles}>
+      <Container>
+        <Row>
+          <Col xs="3">
+            <Button variant="danger">C</Button>
+          </Col>
+          <Col xs="9">
+            <Form.Control
+              type="text"
+              name="txtNumbers"
+              class="text-right"
+              readOnly="readonly"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">7</Button>
+          </Col>
+          <Col>
+            <Button variant="light">8</Button>
+          </Col>
+          <Col>
+            <Button variant="light">9</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">/</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">4</Button>
+          </Col>
+          <Col>
+            <Button variant="light">5</Button>
+          </Col>
+          <Col>
+            <Button variant="light">6</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">*</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">1</Button>
+          </Col>
+          <Col>
+            <Button variant="light">2</Button>
+          </Col>
+          <Col>
+            <Button variant="light">3</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">-</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">0</Button>
+          </Col>
+          <Col>
+            <Button variant="light">.</Button>
+          </Col>
+          <Col>
+            <Button variant="success">=</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">+</Button>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
 }
 
 export default Calculator;
